aa08086fd605e611c0002cd85f351e9a962e6c81
Kwon Mingyu <kmgmg230601@gmail.com>
2023-09-13 14:55:50 +0900

    7785 문제. 근데 풀력하는 방식이 iterator을 이용하는 방법하고 foreach를 쓰는 게 시간차이가 많이나서 합격 불합격을 가를 정도였다

---
+++ Backjoon_7785.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+// 백준 제출용 폼
+int main(int argc, char const* argv[]) {
+    ios::sync_with_stdio(false); // c와 c++ 입력 동기화 끊어서 속도 향상
+    cin.tie(NULL); // cin과 cout의 동기화를 끊어서 속도 향상
+    cout.tie(NULL); // cin과 cout의 동기화를 끊어서 속도 향상
+
+    map<string, string, greater<>> isInOffice;
+
+    int logcount;
+    cin >> logcount;
+
+    string People;
+    string isEnterLeave;
+
+    for (int i = 0; i < logcount; i++)
+    {
+        cin >> People >> isEnterLeave;
+        isInOffice[People] = isEnterLeave;
+    }
+
+
+    // 이거로 출력하면 시간 안에 잘 들어왔지만,
+    map<string, string>::iterator it;
+    for (it = isInOffice.begin(); it != isInOffice.end(); it++) {
+        if (it->second == "enter") {
+            cout << it->first << "\n";
+        }
+    }
+
+    // 이걸로 출력하면 바로 시간 오버가 났다 왜 일까?
+
+    for (auto item : isInOffice)
+    {
+        if (item.second == "enter")
+        {
+            cout << item.first << endl;
+        }
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
