6344ae1fd829c5afb4576ddc038c50590f98fd68
Kwon Mingyu <kmgmg230601@gmail.com>
2023-09-14 13:06:09 +0900

    12789 문제 해결. 마무리 단계 잘 하자

--- BackJoon.cpp
+++ BackJoon.cpp
@@ -14,75 +14,63 @@ int main(int argc, char const* argv[]) {
     cout.tie(NULL); // cin과 cout의 동기화를 끊어서 속도 향상
 
     vector<string> resultVector;
-    stack<char> STACK;
+   
+    int input;
+    int count;
 
-    string input;
-    while (true) {
+    vector<int> LINE;
+    stack<int> STACK;
 
-        getline(cin, input);
-        if (input == ".")
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        cin >> input;
+        LINE.push_back(input);
+    }
+
+    int currentTurn = 1;
+
+    int vectorOutIndex = 0;
+
+    bool flag = true;
+
+    while (currentTurn <= count)
+    {
+
+        if (vectorOutIndex >= LINE.size() && STACK.top() != currentTurn)
+        {
+            flag = false;
             break;
+        }
 
-        while (!STACK.empty()) {
+        if (vectorOutIndex < LINE.size() && LINE[vectorOutIndex] == currentTurn)
+        {
+            vectorOutIndex++;
+            currentTurn++;
+        }
+        else if(!STACK.empty() && STACK.top() == currentTurn)
+        {
             STACK.pop();
-        }
-
-        bool flag = true;
-
-        for (char ch : input)
-        {
-            if (ch == '(')
-            {
-                STACK.push('(');
-            }
-            else if (ch == '[')
-            {
-                STACK.push('[');
-            }
-            else if (ch == ')')
-            {
-                if ((!STACK.empty()) && (STACK.top() == '('))
-                {
-                    STACK.pop();
-                }
-                else
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            else if (ch == ']')
-            {
-                if ((!STACK.empty()) && (STACK.top() == '['))
-                {
-                    STACK.pop();
-                }
-                else
-                {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-
-        if (flag && STACK.empty())
-        {
-            cout << "yes" << endl;
-            //resultVector.push_back("yes");
+            currentTurn++;
         }
         else
         {
-            cout << "no" << endl;
-            //resultVector.push_back("no");
+            STACK.push(LINE[vectorOutIndex]);
+            vectorOutIndex++;
         }
     }
 
+    if (flag)
+    {
+        cout << "Nice";
+    }
+    else
+    {
+        cout << "Sad";
+    }
+    
 
 
-    //for (auto ans : resultVector)
-    //{
-       // cout << ans << endl;
-    //}
-
+    
     return 0;
 }
